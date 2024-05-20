252beb63ad7c600fdf5e511a873224fa8da2ce1c
VarrNguyen <VaarmannJanne961@gmail.com>
2024-05-20 15:59:49 +0700

    Add files via upload

---
+++ src/components/Search/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Policy.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
+
+export default function Search() {
+    return <div>Search</div>;
+}
